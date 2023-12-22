ff31e7da239529fd07168ec225028f97a178df40
thamiresfff <152559104+thamiresfff@users.noreply.github.com>
2023-12-22 00:31:39 +0000

    build:app

--- app.py
+++ app.py
@@ -1,30 +1,12 @@
 import streamlit as st
-#import yfinance as yf
-import pandas as pd
-from data.download import download_data
-from plot.static import plot_line
-from plotnine import *
+from plot. interactive import plot_line_i
 
-st.title('Stock Price App')
+sit.title('Stock Price App')
 
-# Sidebar
-st.sidebar.header('User Input')
-symbol = st.sidebar.text_input('Enter stock symbol (e.g., AAPL):', 'AAPL')
+st. sidebar. header('User Input')
+symbol = st.sidebar. text_input ('Escolha um ativo:', 'APPL')
 
-stock_data = download_data(symbol)
+plot
 
-st.subheader(f'Stock Data for {symbol}')
-st.write(stock_data)
-
-# fig = ggplot(
-#     data = stock_data.reset_index(),
-#     mapping = aes(x = 'Date', y  = 'Close')
-# ) + \
-#     geom_line() + \
-#     ggtitle(f'Dados Históricos do {symbol}') + \
-#     xlab('Data') + \
-#     ylab('Fechamento')
-stock_data = pd.DataFrame(stock_data)
-fig = plot_line(stock_data)
-#st.subheader('Stock Price Chart')
-st.pyplot(fig)
+fig= plot_line_i(symbol)
+st.plotly_char(fig)
